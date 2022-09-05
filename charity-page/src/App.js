a3da4941bcde404275eb4e69a987ce850585bfe8
Petar <stefanovski.p@yahoo.com>
2022-09-05 19:24:12 +0200

    navbar

--- charity-page/src/App.js
+++ charity-page/src/App.js
@@ -3,12 +3,13 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Contact from './components/Contact/Contact';
 import About from './components/About/About';
-
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
     <BrowserRouter >
+    <Navbar />
       <Routes >
         <Route path="/" element={<Home />}/>
         <Route path='/contact' element={<Contact />} />
