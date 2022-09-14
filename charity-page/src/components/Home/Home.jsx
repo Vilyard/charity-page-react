8cf5a6ef54e9c922920ca5719913ab41948f25d7
Petar <stefanovski.p@yahoo.com>
2022-09-14 18:26:30 +0200

    sixth-section-semi-done

--- charity-page/src/components/Home/Home.jsx
+++ charity-page/src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import SecondSection from './Sections/secondSection/secondSection.jsx'
 import ThirdSection from './Sections/thirdSection/thirdSection.jsx'
 import FourthSection from './Sections/fourthSection/fourthSection.jsx'
 import FifthSection from './Sections/fifthSection/fifthSection.jsx'
+import SixthSection from './Sections/sixthSection/sixthSection'
 
 const Home = () => {
 
@@ -17,6 +18,7 @@ const Home = () => {
       < ThirdSection />
       < FourthSection />
       < FifthSection />
+      < SixthSection />
     </div>
   )
 }
---
+++ charity-page/src/components/Home/Sections/sixthSection/sixthSection.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../sixthSection/sixthSection.css'
+
+function sixthSection() {
+  return (
+    <div className='home-sixth-section'>
+            <div className='sixth-container'>
+
+                <div className='sixth-upper-container'>
+
+                <h4>Testimonial</h4>
+                <h2>What People Say About Us</h2>
+                    <p>There are many variations of passages of Lorem Ipsum available, 
+                        but the majority have suffered alteration in some form,</p>
+
+                </div>
+
+                <div className='sixth-lower-container'>
+
+                </div>
+
+            </div>
+        </div>
+  )
+}
+
+export default sixthSection
