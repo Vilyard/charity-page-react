86b1fb79fba79b04f343f664c20e0f0038250b92
Darko <darko.stojanovski64@gmail.com>
2022-09-14 19:20:03 +0200

    changes seventh-section

--- charity-page/src/components/Home/Sections/seventhSection/seventhSection.jsx
+++ charity-page/src/components/Home/Sections/seventhSection/seventhSection.jsx
@@ -6,22 +6,15 @@ function seventhSection() {
   return (
     <div className='home-seventh-section'>
             <div className='seventh-container'>
-
-            
-                    <ul>
-                        <li>
-                            <h1>Lets Chenge The World With Humanity</h1> 
-                        </li>
-                        <li>
-                            <button>Become A Volunteer</button>
-                        </li>
-                        <li>
-                            <img src={volunteerPicture} alt="volunteer picture" id='volunteer-pic' />
-                        </li>
-                    </ul>
-
-              
-
+                <div className='seventh-first-item'>
+                <h1>Lets Chenge The World With Humanity</h1>
+                </div>
+                <div className='seventh-second-item'>
+                <button>Become A Volunteer</button>
+                </div>
+                 <div className='seventh-third-item'>
+                 <img src={volunteerPicture} alt="volunteer picture" id='volunteer-pic' />
+                </div>           
 
             </div>
         </div>
