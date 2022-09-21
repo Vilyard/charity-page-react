db569b83a72168b3f149ead893d76e4b62a6c14a
Darko <darko.stojanovski64@gmail.com>
2022-09-21 18:42:44 +0200

    fifth-section-carousel-added

---
+++ charity-page/src/components/Sliders/FifthSectionSliderData.jsx
@@ -0,0 +1,28 @@
+import fifthSectionFirstPic from "../Images/fifth-carousel-pic1.jpg"
+import fifthSectionSecoundPic from "../Images/fifth-carousel-pic2.jpg"
+import fifthSectionThirdPic from "../Images/fifth-carousel-pic3.jpg"
+import fifthSectionFourthPic from "../Images/fifth-carousel-pic4.jpg"
+import fifthSectionFifthPic from "../Images/fifth-carousel-pic5.jpg"
+import fifthSectionSixthPic from "../Images/fifth-carousel-pic6.jpg"
+
+
+export const FifthSectionSilderData = [
+    {
+        image: fifthSectionFirstPic
+    },
+    {
+        image: fifthSectionSecoundPic
+    },
+    {
+        image: fifthSectionThirdPic
+    },
+    {
+        image: fifthSectionFourthPic
+    },
+    {
+        image: fifthSectionFifthPic 
+    },
+    {
+        image: fifthSectionSixthPic 
+    }
+]
