7959cd707236642a97fcdbb19f49a44093700c2b
Darko <darko.stojanovski64@gmail.com>
2022-09-21 18:59:00 +0200

    sliderdata+initial-slider

---
+++ charity-page/src/components/Sliders/SixthSectionSlider.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+import "../Sliders/SithSectionSlider.css"
+import { SixthSectionSliderData } from './SixthSectionSliderData'
+
+const SixthSectionSlider = () => {
+  return (
+    <div>SixthSectionSlider</div>
+  )
+}
+
+export default SixthSectionSlider
