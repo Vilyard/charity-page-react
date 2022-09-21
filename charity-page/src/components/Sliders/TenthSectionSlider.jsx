afa7e8d2e4c650f29ad8d0d05a807fe171af8bdb
Darko <darko.stojanovski64@gmail.com>
2022-09-21 19:09:03 +0200

    added images + jsx

---
+++ charity-page/src/components/Sliders/TenthSectionSlider.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+import "../Sliders/TenthectionSlider.css"
+import { TenthSectionSliderData } from './TenthSectionSliderData'
+
+const TenthSectionSlider = () => {
+  return (
+    <div>TenthSectionSlider</div>
+  )
+}
+
+export default TenthSectionSlider
---
+++ charity-page/src/components/Sliders/TenthSectionSliderData.jsx
@@ -0,0 +1,25 @@
+import tenthSectionFirstPic from "../Images/tenth-carousel-pic1.jfif"
+import tenthSectionSecoundPic from "../Images/tenth-carousel-pic2.jfif"
+import tenthSectionThirdPic from "../Images/tenth-carousel-pic3.jfif"
+import tenthSectionFourthPic from "../Images/tenth-carousel-pic4.jfif"
+import tenthSectionFifthPic from "../Images/tenth-carousel-pic5.jfif"
+
+
+
+export const TenthSectionSilderData = [
+    {
+        image: tenthSectionFirstPic
+    },
+    {
+        image: tenthSectionSecoundPic
+    },
+    {
+        image: tenthSectionThirdPic,
+    },
+    {
+        image: tenthSectionFourthPic,
+    },
+    {
+        image: tenthSectionFifthPic,
+    }
+]
