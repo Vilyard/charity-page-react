43e1ba880ff183667c2bf54e8b2e3757f2ac9c6c
Petar <stefanovski.p@yahoo.com>
2022-09-27 16:16:59 +0200

    second-section-done

--- charity-page/src/components/About/About.jsx
+++ charity-page/src/components/About/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "../About/About.css"
 import FirstSectionAbout from '../About/Sections/firstSectionAbout/firstSectionAbout.jsx'
+import SecondSectionAbout from './Sections/secondSectionAbout/secondSectionAbout'
 
 const About = () => {
   return (
     <div className='about-container'>
 
       < FirstSectionAbout />
+      < SecondSectionAbout />
 
     </div>
   )
