c392e6de32e094d0d9871c261a4620c040f27fe6
Darko Stojanovski <96201123+Vilyard@users.noreply.github.com>
2022-09-29 18:37:00 +0200

    quick-fix

--- charity-page/src/components/Home/Sections/firstSection/firstSection.jsx
+++ charity-page/src/components/Home/Sections/firstSection/firstSection.jsx
@@ -4,38 +4,27 @@ import kid1 from '../../../Images/kid1.png'
 import totalProjects from '../../../Images/totalprojects.png'
 import kid2 from '../../../Images/kid2.png'
 import FirstSectionSlider from '../../../Sliders/FirstSectionSlider.jsx'
-import { FirstSectionSilderData } from '../../../Sliders/FirstSectionSilderData.jsx'
-import  SecondSlider  from '../../../Sliders/SecondSlider.jsx'
-
 function firstSection() {
   return (
-   
 <div className='home-first-section'>
-
 <div className='first-section-first-div'>
     <h3>Help The Poor Children</h3>
     <h1>Your Marcy Can Change Someone’s Life</h1>
     <p>We help local nonprofits access the funding, tools, training, and support they need to become more.</p>
     <button>Get Started</button>
   </div>
-
   <div className='first-section-second-div'>
     <img src={kid1} alt="" />
     <div className='sliders-component'>
-    < FirstSectionSlider items={FirstSectionSilderData} />
-    < SecondSlider items = {FirstSectionSilderData} />
+    < FirstSectionSlider />
     </div>
-
   </div>
-
   <div className='first-section-third-div'>
-    <img src={totalProjects} alt=""/>
+    <img src={totalProjects} id="totalprojectsPic"  alt=""/>
     <br />
-    <img src={kid2} alt="" />
+    <img src={kid2}alt="" />
   </div>
 </div>
-  
   )
 }
-
 export default firstSection
--- charity-page/src/components/Sliders/FirstSectionSlider.jsx
+++ charity-page/src/components/Sliders/FirstSectionSlider.jsx
@@ -1,93 +1,27 @@
-import React, {useState, useEffect} from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
 import "../Sliders/FirstSectionSlider.css"
-import { FirstSectionSilderData } from './FirstSectionSilderData'
-
-
-const FirstSectionSlider = ({items}) => {
-    const [firstImage, setFirstImage] = useState(0);
-    const [secondImage, setSecondImage] = useState (1);
-    const [thirdImage, setThirdImageImage] = useState(2);
-    const [fourthImage, setFourthImage] = useState (3);
-
-    const length = items.length;
-    
-    useEffect(
-        () => {
-            const interval = setInterval(() => {
-                setFirstImage(firstImage === length - 1 ? 0 : firstImage + 1);
-                setSecondImage(secondImage === length - 1 ? 0 : secondImage + 1);
-                setThirdImageImage(thirdImage === length - 1 ? 0 : thirdImage + 1);
-                setFourthImage(fourthImage === length - 1 ? 0 : fourthImage + 1);
-                console.log(firstImage)
-                console.log(secondImage)
-                console.log(thirdImage)
-            }, 2000);
-            return () => clearInterval(interval);
-        },
-        [firstImage], [secondImage], [thirdImage]
-    );
-
-  
-
-   
-
-
-    // const nextItem = () => {
-    //     setCurrentImage(currentImage === length - 1 ? 0 : currentImage + 1)
-    // }
-    // const previousItem = () => {
-    //     setCurrentImage(currentImage === 0 ? length - 1 : currentImage -1)
-    // }
-
-    if(!Array.isArray(items) || items.length <= 0) {
-        return null;
-    }
-    
+import FirstSectionSliderData from '../Sliders/FirstSectionSliderData.jsx'
+const FirstSectionSlider = () => {
+  const [width, setWidth] = useState(0);
+  const firstCarousel = useRef();
+  useEffect(() => {
+    setWidth(firstCarousel.current.scrollWidth - firstCarousel.current.offsetWidth)
+  }, [])
   return (
-    <div className='first-section-slider'>
-        {/* <button className='left-btn' onClick={previousItem}>left</button>
-        <button className='right-btn' onClick={nextItem}>right</button> */}
-        {FirstSectionSilderData.map((items, index) => {
+    <div>
+      <motion.div ref={firstCarousel} className='firstCarousel' whileTap={{cursor: "grabbing"}}>
+        <motion.div drag='x' dragConstraints={{right: 0, left: -width}} className='inner-first-carousel'>
+          {FirstSectionSliderData.map(type => {
             return (
-                <div>
-                <div className={index === firstImage ? "item-active" : "item"} key={index}>
-                    {index === firstImage && 
-                        (  
-                            <img src={items.image} alt="test" className='first-section-img' />
-                            
-                        )
-                    }    
-    
-                     </div>
-                     <div className={index === secondImage ? "item-active" : "item"} key={index}>
-                    {index === secondImage && 
-                        (  
-                            <img src={items.image} alt="test" className='first-section-img' />
-                            
-                        )
-                    }    
-    
-                     </div>
-
-                     <div className={index === thirdImage ? "item-active" : "item"} key={index}>
-                    {index === thirdImage && 
-                        (  
-                            <img src={items.image} alt="test" className='first-section-img' />
-                            
-                        )
-                    }    
-    
-                     </div>
-
-                   
-                 </div>
-
-                 
-                
-            )
-        })}
+              <motion.div className='item-firstSection' key={type}>
+                <img src={type.image} alt='test' />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
-
 export default FirstSectionSlider
