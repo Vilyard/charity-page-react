28d96b29ee278e93c3979eb149e85e84f15a47ed
Darko <darko.stojanovski64@gmail.com>
2022-09-29 18:38:57 +0200

    bug-test

--- charity-page/src/components/Home/Sections/firstSection/firstSection.jsx
+++ charity-page/src/components/Home/Sections/firstSection/firstSection.jsx
@@ -8,7 +8,7 @@ function firstSection() {
   return (
 <div className='home-first-section'>
 <div className='first-section-first-div'>
-    <h3>Help The Poor Children</h3>
+    <h3>Help The Poor Asd Children</h3>
     <h1>Your Marcy Can Change Someone’s Life</h1>
     <p>We help local nonprofits access the funding, tools, training, and support they need to become more.</p>
     <button>Get Started</button>
