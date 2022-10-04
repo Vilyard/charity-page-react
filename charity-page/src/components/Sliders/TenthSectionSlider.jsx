36c17de4aa4a58a4d4b1591826b8496586eccddc
Petar <stefanovski.p@yahoo.com>
2022-10-04 19:59:35 +0200

    tenthSectionCarousel-test1

--- charity-page/src/components/Sliders/TenthSectionSlider.jsx
+++ charity-page/src/components/Sliders/TenthSectionSlider.jsx
@@ -33,25 +33,26 @@
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import FirstSectionSliderData from '../Sliders/FirstSectionSliderData.jsx'
-import "../Sliders/FirstSectionSlider.css"
-function FirstSectionSlider() {
-   const items = FirstSectionSliderData.map(type => {
+import TenthSectionSliderData from '../Sliders/TenthSectionSliderData.jsx'
+import "../Sliders/TenthSectionSlider.css"
+function TenthSectionSlider() {
+   const items = TenthSectionSliderData.map(type => {
                 return (
-                  <div className='item-firstSection' key={type}>
+                  <div className='item-tenthSection' key={type}>
                     <img src={type.image} alt='test' />
                   </div>
                 );
               })
   return (
-    <div className='first-carousel-container'>
+    <div className='tenth-carousel-container'>
       <AliceCarousel mouseTracking items={items} responsive={{0: {items:1}, 767: {items: 3}, 1023: {items: 5}}}
        disableDotsControls={true} disableButtonsControls={true} infinite={true} autoPlay={true} autoPlayInterval={3000}
+       autoWidth={true}
 >
       </AliceCarousel>
     </div>
   )
 }
-export default FirstSectionSlider
+export default TenthSectionSlider
 
 
