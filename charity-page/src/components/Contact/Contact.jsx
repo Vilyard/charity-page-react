bb2499772c8414aa773e238447d55c6722e808cd
Darko <darko.stojanovski64@gmail.com>
2022-10-08 18:56:17 +0200

    fourth+third

--- charity-page/src/components/Contact/Contact.jsx
+++ charity-page/src/components/Contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import FirstSectionContact from './Sections/firstSectionContact/firstSectionContact'
 import SecondSectionContact from './Sections/secondSectionContact/secondSectionContact'
+import ThirdSectionContact from './Sections/thirdSectionContact/thirdSectionContact'
+import FourthSectionContact from './Sections/fourthSectionContact/fourthSectionContact'
 import "../Contact/Contact.css"
 const Contact = () => {
   return (
@@ -8,8 +10,8 @@ const Contact = () => {
 
     < FirstSectionContact />
     < SecondSectionContact />
-    {/* < ThirdSectionContact /> 
-   < FourthSectionContact /> */}
+    < ThirdSectionContact /> 
+   < FourthSectionContact />
    
   </div>
   )
