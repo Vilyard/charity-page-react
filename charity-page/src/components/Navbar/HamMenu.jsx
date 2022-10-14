147b38dc3032fff96e5c088b5f2cbe8a73c1ff72
Darko <darko.stojanovski64@gmail.com>
2022-10-14 19:55:33 +0200

    added-links

--- charity-page/src/components/Navbar/HamMenu.jsx
+++ charity-page/src/components/Navbar/HamMenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../Navbar/HamMenu.css"
+import { Link } from "react-router-dom"
 
 function HamMenu() {
   return (
@@ -16,9 +17,9 @@ function HamMenu() {
     
 
     <ul id="menu">
-      <a href="#"><li>Home</li></a>
-      <a href="#"><li>About</li></a>
-      <a href="#"><li>Contact</li></a>
+      <Link to="home"><a href="#"><li>Home</li></a></Link>
+      <Link to="about"><a href="#"><li>About</li></a></Link>
+      <Link to="contact"><a href="#"><li>Contact</li></a></Link>
     </ul>
   </div>
 
