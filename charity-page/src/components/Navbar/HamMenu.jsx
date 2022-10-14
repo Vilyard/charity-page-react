e0c363c6101816bc0fc2030dcc94658d9db38b0a
Petar <stefanovski.p@yahoo.com>
2022-10-14 19:50:00 +0200

    hamburger-menu-done

--- charity-page/src/components/Navbar/HamMenu.jsx
+++ charity-page/src/components/Navbar/HamMenu.jsx
@@ -1,38 +1,29 @@
 import React from 'react'
-import {useState} from "react"
 import "../Navbar/HamMenu.css"
-import {GiHamburgerMenu} from "react-icons/gi"
-import { AiOutlineClose } from "react-icons/ai"
 
-const HamMenu = () => {
-  const [menu, setMenu] = useState(false);
-
-
-  const openMenu = () =>{
-    setMenu(!menu);
-  }
-
-
+function HamMenu() {
   return (
-    <div className='ham-menu-container'>
-        <button id='ham-menu-btn' onClick={openMenu}> <GiHamburgerMenu /> 
-        {menu ? (
-            <section className='menu__bg'>
-                <div className='menu__align'>
-                  <div className='menu__content' menu={menu}>
-                    <button id="ham_menu_close_btn">
-                    < AiOutlineClose className="menu__close" />
-                    </button>
-                      <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                      </ul>
-                  </div>
-                </div>
-            </section>
-        ) : null}
-        </button>
+    <div>
+
+
+  <div id="menuToggle">
+  
+    <input type="checkbox" />
+    
+   <span></span>
+    <span></span>
+    <span></span> 
+    
+
+    <ul id="menu">
+      <a href="#"><li>Home</li></a>
+      <a href="#"><li>About</li></a>
+      <a href="#"><li>Contact</li></a>
+    </ul>
+  </div>
+
+      
+
     </div>
   )
 }
