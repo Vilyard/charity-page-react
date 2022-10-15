66d662b85998be31ccd4a9e823bb10d31615c7dc
Darko <darko.stojanovski64@gmail.com>
2022-10-15 15:51:23 +0200

    home-2nd-half-formated

--- charity-page/src/components/Home/Sections/sixthSection/sixthSection.jsx
+++ charity-page/src/components/Home/Sections/sixthSection/sixthSection.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import '../sixthSection/sixthSection.css'
-import SixthSectionSlider from '../../../Sliders/SixthSectionSlider.jsx'
+import React from "react";
+import "../sixthSection/sixthSection.css";
+import SixthSectionSlider from "../../../Sliders/SixthSectionSlider.jsx";
 function sixthSection() {
   return (
-    <div className='home-sixth-section'>
-            <div className='sixth-container'>
-                <div className='sixth-upper-container'>
-                <h4>Testimonial</h4>
-                <h2>What People Say About Us</h2>
-                    <p>There are many variations of passages of Lorem Ipsum available,
-                        but the majority have suffered alteration in some form,</p>
-                </div>
-                <div className='sixth-lower-container'>
-                < SixthSectionSlider />
-                </div>
-            </div>
+    <div className="home-sixth-section">
+      <div className="sixth-container">
+        <div className="sixth-upper-container">
+          <h4>Testimonial</h4>
+          <h2>What People Say About Us</h2>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form,
+          </p>
         </div>
-  )
+        <div className="sixth-lower-container">
+          <SixthSectionSlider />
+        </div>
+      </div>
+    </div>
+  );
 }
-export default sixthSection
+export default sixthSection;
--- charity-page/src/components/Home/Sections/tenthSection/tenthSection.jsx
+++ charity-page/src/components/Home/Sections/tenthSection/tenthSection.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import '../tenthSection/tenthSection.css'
-import TenthSectionSlider from '../../../Sliders/TenthSectionSlider.jsx'
+import React from "react";
+import "../tenthSection/tenthSection.css";
+import TenthSectionSlider from "../../../Sliders/TenthSectionSlider.jsx";
 function tenthSection() {
   return (
-    <div className='home-tenth-section'>
-    <div className='tenth-container'>
-        <div className='tenth-upper-container'>
-            <h4>Who help us</h4>
-            <h2>Our Partners & Donors</h2>
-            <p>There are many variations of passages of Lorem Ipsum available,
-                but the majority have suffered alteration in some form,</p>
+    <div className="home-tenth-section">
+      <div className="tenth-container">
+        <div className="tenth-upper-container">
+          <h4>Who help us</h4>
+          <h2>Our Partners & Donors</h2>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form,
+          </p>
         </div>
-        <div className='tenth-lower-container'>
-                    < TenthSectionSlider />
-                </div>
+        <div className="tenth-lower-container">
+          <TenthSectionSlider />
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
-export default tenthSection
+export default tenthSection;
