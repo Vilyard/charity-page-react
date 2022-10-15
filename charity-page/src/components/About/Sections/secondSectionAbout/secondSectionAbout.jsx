44d78c7a89c09476f9cc7598fccd2b2d367ac8db
Darko <darko.stojanovski64@gmail.com>
2022-10-15 15:34:12 +0200

    test-1

--- charity-page/src/components/About/Sections/secondSectionAbout/secondSectionAbout.jsx
+++ charity-page/src/components/About/Sections/secondSectionAbout/secondSectionAbout.jsx
@@ -1,33 +1,27 @@
-import React from 'react'
-import '../secondSectionAbout/secondSectionAbout.css'
-import SeconSectionPic from '../../../Images/second-section-about-picture.jpg'
-import Modal from "../secondSectionAbout/popupSecondSection.jsx"
+import React from "react";
+import "../secondSectionAbout/secondSectionAbout.css";
+import SeconSectionPic from "../../../Images/second-section-about-picture.jpg";
+import Modal from "../secondSectionAbout/popupSecondSection.jsx";
 
 function secondSectionAbout() {
-    return (
-        <div className='about-second-section'>
-
-            <div className='second-section-container'>
-
-
-                <div className='image-container-second-about-section'>
-
-                    <img src={SeconSectionPic} alt="second-section-picture" />
-                    <div className='ghana-video'>
-                    < Modal />
-                  
-                    </div>
-
-                </div>
-
-                <h2>See what we do for the <span>poor people and the children</span></h2>
-
-                <hr />
-
-            </div>
-
+  return (
+    <div className="about-second-section">
+      <div className="second-section-container">
+        <div className="image-container-second-about-section">
+          <img src={SeconSectionPic} alt="second-section-picture" />
+          <div className="ghana-video">
+            <Modal />
+          </div>
         </div>
-    )
+
+        <h2>
+          See what we do for the <span>poor people and the children</span>
+        </h2>
+
+        <hr />
+      </div>
+    </div>
+  );
 }
 
-export default secondSectionAbout
+export default secondSectionAbout;
